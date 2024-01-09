ffb70ddffc3068a6cb12873d946273d1d928a306
Luziv <86086748+DiaeEddineJamal@users.noreply.github.com>
2024-01-09 11:29:28 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -59,23 +59,9 @@ CREATE TABLE `friends` (
 
 LOCK TABLES `friends` WRITE;
 /*!40000 ALTER TABLE `friends` DISABLE KEYS */;
-INSERT INTO `friends` VALUES ('hohos',0,0),('smt_coder',0,0),('eXperienced',0,0),('satan_nightmare',0,0),('gg_14',1,1);
+INSERT INTO `friends` VALUES ('hohos',0,0),('smt_coder',0,0),('eXperienced',0,0),('satan_nightmare',0,0);
 /*!40000 ALTER TABLE `friends` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `invitations`
---
-
-DROP TABLE IF EXISTS `invitations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `invitations` (
-  `fromuser` varchar(70) DEFAULT NULL,
-  `touser` varchar(70) DEFAULT NULL,
-  `type` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `invitations`
@@ -83,29 +69,9 @@ CREATE TABLE `invitations` (
 
 LOCK TABLES `invitations` WRITE;
 /*!40000 ALTER TABLE `invitations` DISABLE KEYS */;
-INSERT INTO `invitations` VALUES ('Friends','hohos',0),('Friends','smt_coder',0),('Friends','eXperienced',0),('Friends','satan_nightmare',0),('party','alimente',0),('party','dizay',0),('party','luziver',0),('Secret_Group','yazid',0),('Secret_Group','hohos',0),('Secret_Group','smt_coder',0),('Secret_Group','gg_14',0),('Secret_Group','eXperienced',0),('Secret_Group','satan_nightmare',0),('Secret_Group','dizay',0),('Secret_Group','alimente',0);
+INSERT INTO `invitations` VALUES ('Friends','hohos',0),('Friends','smt_coder',0),('Friends','eXperienced',0),('Friends','satan_nightmare',0),('party','alimente',0),('party','dizay',0),('party','luziver',0),('Secret_Group','yazid',0),('Secret_Group','hohos',0),('Secret_Group','smt_coder',0),('Secret_Group','eXperienced',0),('Secret_Group','satan_nightmare',0),('Secret_Group','dizay',0),('Secret_Group','alimente',0);
 /*!40000 ALTER TABLE `invitations` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `messages`
---
-
-DROP TABLE IF EXISTS `messages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `messages` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
-  `messagefrom` varchar(70) DEFAULT NULL,
-  `messageto` varchar(70) DEFAULT NULL,
-  `message` varchar(500) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  `sent` tinyint(1) DEFAULT NULL,
-  `type` tinyint(1) DEFAULT NULL,
-  `actualsender` varchar(70) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=451 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `messages`
@@ -113,47 +79,9 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES (383,'alimente','yazid','whats up captain?','2017-12-21 06:45:26',1,0,'alimente'),(384,'yazid','alimente','nothing much','2017-12-21 06:45:57',1,0,'yazid'),(385,'yazid','alimente','just daily routine','2017-12-21 06:46:05',1,0,'yazid'),(386,'alimente','yazid','what about a fight...hahahaha','2017-12-21 06:46:39',1,0,'alimente'),(387,'yazid','alimente','i dont think','2017-12-21 06:46:54',1,0,'yazid'),(388,'yazid','alimente','u can control yourself','2017-12-21 06:47:06',1,0,'yazid'),(389,'gg_14','alimente','hey alimente!!','2017-12-21 06:52:36',0,0,'gg_14'),(390,'gg_14','alimente','having fun?','2017-12-21 06:52:43',0,0,'gg_14'),(391,'gg_14','alimente','in controlling yourself??','2017-12-21 06:52:52',0,0,'gg_14'),(392,'gg_14','alimente','lol lol lol','2017-12-21 06:53:02',0,0,'gg_14'),(393,'smt_coder','alimente','its been a month','2017-12-21 06:53:20',0,0,'smt_coder'),(394,'smt_coder','alimente','since i last the real u','2017-12-21 06:53:31',0,0,'smt_coder'),(395,'smt_coder','alimente','time to turn green','2017-12-21 06:53:39',0,0,'smt_coder'),(396,'smt_coder','alimente','huh??','2017-12-21 06:53:41',0,0,'smt_coder'),(397,'smt_coder','alimente','hahaha','2017-12-21 06:53:44',0,0,'smt_coder'),(398,'hohos','alimente','I am a philosopher','2017-12-21 06:54:07',1,0,'hohos'),(399,'hohos','alimente','tell me ur problems','2017-12-21 06:54:14',1,0,'hohos'),(400,'hohos','alimente','and i will solve them','2017-12-21 06:54:28',1,0,'hohos'),(401,'yazid','alimente','but still','2017-12-21 06:54:45',1,0,'yazid'),(402,'yazid','alimente','I am always up for fight!!','2017-12-21 06:54:57',1,0,'yazid'),(403,'gg_14','eXperienced','Preparing well for ICPC?','2017-12-21 07:03:44',0,0,'gg_14'),(404,'gg_14','eXperienced','All the best brother','2017-12-21 07:03:53',0,0,'gg_14'),(405,'alimente','yazid','just tell the venue..','2017-12-21 07:08:37',1,0,'alimente'),(406,'yazid','alimente','where I was born','2017-12-21 07:08:55',1,0,'yazid'),(407,'alimente','yazid','that was a century ago','2017-12-21 07:09:16',1,0,'alimente'),(408,'alimente','yazid','you emotional idiot','2017-12-21 07:09:32',1,0,'alimente'),(409,'hohos','yazid','hey','2017-12-21 07:10:56',1,0,'hohos'),(410,'hohos','yazid','cap','2017-12-21 07:10:58',1,0,'hohos'),(411,'hohos','yazid','howdy','2017-12-21 07:11:04',1,0,'hohos'),(412,'alimente','dizay','I am gonna rip you in two pieces','2017-12-21 07:26:29',1,0,'alimente'),(413,'hackers','yazid','I will beat u all','2017-12-21 07:55:28',1,0,'dizay'),(414,'hackers','luziver','I will beat u all','2017-12-21 07:55:28',1,0,'dizay'),(415,'hackers','alimente','I will beat u all','2017-12-21 07:55:28',1,0,'dizay'),(416,'dizay','hackers','I will beat u all','2017-12-21 07:55:28',0,0,'dizay'),(417,'hackers','yazid','how you people are gonna fight with me??','2017-12-21 07:56:03',1,0,'dizay'),(418,'hackers','luziver','how you people are gonna fight with me??','2017-12-21 07:56:03',1,0,'dizay'),(419,'hackers','alimente','how you people are gonna fight with me??','2017-12-21 07:56:03',1,0,'dizay'),(420,'dizay','hackers','how you people are gonna fight with me??','2017-12-21 07:56:03',0,0,'dizay'),(421,'hackers','yazid','like the old man say..','2017-12-21 07:56:57',1,0,'luziver'),(422,'hackers','dizay','like the old man say..','2017-12-21 07:56:57',1,0,'luziver'),(423,'hackers','alimente','like the old man say..','2017-12-21 07:56:57',1,0,'luziver'),(424,'luziver','hackers','like the old man say..','2017-12-21 07:56:57',0,0,'luziver'),(425,'hackers','yazid','TOGETHER','2017-12-21 07:57:05',1,0,'luziver'),(426,'hackers','dizay','TOGETHER','2017-12-21 07:57:05',1,0,'luziver'),(427,'hackers','alimente','TOGETHER','2017-12-21 07:57:05',1,0,'luziver'),(428,'luziver','hackers','TOGETHER','2017-12-21 07:57:05',0,0,'luziver'),(429,'hackers','yazid','Hell Yeah!! ','2017-12-21 07:57:30',1,0,'alimente'),(430,'hackers','dizay','Hell Yeah!! ','2017-12-21 07:57:30',1,0,'alimente'),(431,'hackers','luziver','Hell Yeah!! ','2017-12-21 07:57:30',1,0,'alimente'),(432,'alimente','hackers','Hell Yeah!! ','2017-12-21 07:57:30',0,0,'alimente'),(433,'hackers','yazid','src/resources/server_resources/ChitChat_5MA3J.jpg','2017-12-21 07:59:53',0,1,'luziver'),(434,'hackers','dizay','src/resources/server_resources/ChitChat_5MA3J.jpg','2017-12-21 07:59:53',0,1,'luziver'),(435,'hackers','alimente','src/resources/server_resources/ChitChat_5MA3J.jpg','2017-12-21 07:59:53',0,1,'luziver'),(436,'luziver','hackers','src/resources/server_resources/ChitChat_5MA3J.jpg','2017-12-21 07:59:53',0,1,'luziver'),(437,'hohos','yazid','Why are you not replying?','2017-12-21 08:21:22',0,0,'hohos'),(438,'hohos','yazid','I am waiting for the response...','2017-12-21 08:21:36',0,0,'hohos'),(439,'smt_coder','yazid','Hey cap','2017-12-21 08:21:54',0,0,'smt_coder'),(440,'smt_coder','yazid','try coding','2017-12-21 08:21:59',0,0,'smt_coder'),(441,'smt_coder','yazid','coding is the future','2017-12-21 08:22:07',0,0,'smt_coder'),(442,'gg_14','yazid','whats your strategy to beat dizay?','2017-12-21 08:22:28',0,0,'gg_14'),(443,'gg_14','yazid','If u want...I can help u in that..','2017-12-21 08:22:46',0,0,'gg_14'),(444,'luziver','yazid','hey cap','2017-12-21 08:23:25',0,0,'luziver'),(445,'luziver','yazid','sorry for what happened in civil war','2017-12-21 08:23:37',0,0,'luziver'),(446,'luziver','yazid','hope u understand','2017-12-21 08:23:53',0,0,'luziver'),(447,'hackers','dizay','u just wait and watch your end..dizay','2017-12-21 08:26:17',0,0,'yazid'),(448,'hackers','luziver','u just wait and watch your end..dizay','2017-12-21 08:26:17',1,0,'yazid'),(449,'hackers','alimente','u just wait and watch your end..dizay','2017-12-21 08:26:17',0,0,'yazid'),(450,'yazid','hackers','u just wait and watch your end..dizay','2017-12-21 08:26:17',0,0,'yazid');
+INSERT INTO `messages` VALUES (383,'alimente','yazid','whats up captain?','2023-12-21 06:45:26',1,0,'alimente'),(384,'yazid','alimente','nothing much','2023-12-21 06:45:57',1,0,'yazid'),(385,'yazid','alimente','just daily routine','2023-12-21 06:46:05',1,0,'yazid'),(386,'alimente','yazid','what about a fight...hahahaha','2023-12-21 06:46:39',1,0,'alimente'),(387,'yazid','alimente','i dont think','2023-12-21 06:46:54',1,0,'yazid'),(388,'yazid','alimente','u can control yourself','2023-12-21 06:47:06',1,0,'yazid'),(393,'smt_coder','alimente','its been a month','2023-12-21 06:53:20',0,0,'smt_coder'),(394,'smt_coder','alimente','since i last the real u','2023-12-21 06:53:31',0,0,'smt_coder'),(395,'smt_coder','alimente','time to turn green','2023-12-21 06:53:39',0,0,'smt_coder'),(396,'smt_coder','alimente','huh??','2023-12-21 06:53:41',0,0,'smt_coder'),(397,'smt_coder','alimente','hahaha','2023-12-21 06:53:44',0,0,'smt_coder'),(398,'hohos','alimente','I am a philosopher','2023-12-21 06:54:07',1,0,'hohos'),(399,'hohos','alimente','tell me ur problems','2023-12-21 06:54:14',1,0,'hohos'),(400,'hohos','alimente','and i will solve them','2023-12-21 06:54:28',1,0,'hohos'),(401,'yazid','alimente','but still','2023-12-21 06:54:45',1,0,'yazid'),(402,'yazid','alimente','I am always up for fight!!','2023-12-21 06:54:57',1,0,'yazid'),(403,'gg_14','eXperienced','Preparing well for ICPC?','2023-12-21 07:03:44',0,0,'gg_14'),(404,'gg_14','eXperienced','All the best brother','2023-12-21 07:03:53',0,0,'gg_14'),(405,'alimente','yazid','just tell the venue..','2023-12-21 07:08:37',1,0,'alimente'),(406,'yazid','alimente','where I was born','2023-12-21 07:08:55',1,0,'yazid'),(407,'alimente','yazid','that was a century ago','2023-12-21 07:09:16',1,0,'alimente'),(408,'alimente','yazid','you emotional idiot','2023-12-21 07:09:32',1,0,'alimente'),(409,'hohos','yazid','hey','2023-12-21 07:10:56',1,0,'hohos'),(410,'hohos','yazid','cap','2023-12-21 07:10:58',1,0,'hohos'),(411,'hohos','yazid','howdy','2023-12-21 07:11:04',1,0,'hohos'),(412,'alimente','dizay','I am gonna rip you in two pieces','2023-12-21 07:26:29',1,0,'alimente'),(413,'hackers','yazid','I will beat u all','2023-12-21 07:55:28',1,0,'dizay'),(414,'hackers','luziver','I will beat u all','2023-12-21 07:55:28',1,0,'dizay'),(415,'hackers','alimente','I will beat u all','2023-12-21 07:55:28',1,0,'dizay'),(416,'dizay','hackers','I will beat u all','2023-12-21 07:55:28',0,0,'dizay'),(417,'hackers','yazid','how you people are gonna fight with me??','2023-12-21 07:55:52',0,0,'hackers'),(418,'yazid','hackers','Just watch','2023-12-21 07:56:14',0,0,'yazid'),(419,'yazid','hackers','I will show you my skills','2023-12-21 07:56:34',0,0,'yazid'),(420,'hackers','yazid','you will be beaten in first round','2023-12-21 07:57:00',0,0,'hackers'),(421,'yazid','hackers','Let the games begin','2023-12-21 07:57:23',0,0,'yazid'),(422,'hackers','yazid','good luck to you','2023-12-21 07:57:42',0,0,'hackers'),(423,'gg_14','yazid','A game is on','2023-12-21 08:09:25',0,0,'gg_14'),(424,'gg_14','hohos','All the best guys','2023-12-21 08:09:41',0,0,'gg_14'),(425,'hohos','gg_14','we will win for sure','2023-12-21 08:09:55',0,0,'hohos'),(426,'gg_14','hohos','Do your best','2023-12-21 08:10:09',0,0,'gg_14'),(427,'hohos','gg_14','Yeah','2023-12-21 08:10:22',0,0,'hohos'),(428,'gg_14','hohos','we are gonna rock it','2023-12-21 08:10:38',0,0,'gg_14'),(429,'gg_14','hohos','Hey','2023-12-21 08:10:50',0,0,'gg_14'),(430,'hohos','gg_14','Hey','2023-12-21 08:10:59',0,0,'hohos'),(431,'hohos','gg_14','Let\'s meet after the match','2023-12-21 08:11:13',0,0,'hohos'),(432,'gg_14','hohos','Sure','2023-12-21 08:11:30',0,0,'gg_14'),(433,'gg_14','hohos','celebrations awaiting','2023-12-21 08:11:43',0,0,'gg_14'),(434,'hohos','gg_14','victory!!','2023-12-21 08:11:56',0,0,'hohos'),(435,'gg_14','hohos','Yeah!!','2023-12-21 08:12:09',0,0,'gg_14'),(436,'hohos','gg_14','Party time','2023-12-21 08:12:23',0,0,'hohos'),(437,'gg_14','hohos','Hell yeah','2023-12-21 08:12:39',0,0,'gg_14'),(438,'gg_14','hohos','Celebrate the victory','2023-12-21 08:12:53',0,0,'gg_14'),(439,'gg_14','hohos','We did it','2023-12-21 08:13:10',0,0,'gg_14'),(440,'hohos','gg_14','Cheers','2023-12-21 08:13:26',0,0,'hohos'),(441,'gg_14','hohos','Cheers','2023-12-21 08:13:41',0,0,'gg_14');
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `party`
---
-
-DROP TABLE IF EXISTS `party`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `party` (
-  `participants` varchar(70) DEFAULT NULL,
-  `isAdmin` tinyint(1) DEFAULT NULL,
-  `accepted` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `party`
---
-
-LOCK TABLES `party` WRITE;
-/*!40000 ALTER TABLE `party` DISABLE KEYS */;
-INSERT INTO `party` VALUES ('alimente',0,0),('dizay',0,0),('luziver',0,0),('yazid',1,1);
-/*!40000 ALTER TABLE `party` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `secret_group`
---
-
-DROP TABLE IF EXISTS `secret_group`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `secret_group` (
-  `participants` varchar(70) DEFAULT NULL,
-  `isAdmin` tinyint(1) DEFAULT NULL,
-  `accepted` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `secret_group`
@@ -161,13 +89,9 @@ CREATE TABLE `secret_group` (
 
 LOCK TABLES `secret_group` WRITE;
 /*!40000 ALTER TABLE `secret_group` DISABLE KEYS */;
-INSERT INTO `secret_group` VALUES ('yazid',0,0),('hohos',0,0),('smt_coder',0,0),('gg_14',0,0),('eXperienced',0,0),('satan_nightmare',0,0),('dizay',0,0),('alimente',0,0),('luziver',1,1);
+INSERT INTO `secret_group` VALUES ('Party','alimente',0),('Party','dizay',0),('Party','luziver',0),('Secret_Group','yazid',0),('Secret_Group','hohos',0),('Secret_Group','smt_coder',0),('Secret_Group','eXperienced',0),('Secret_Group','satan_nightmare',0),('Secret_Group','dizay',0),('Secret_Group','alimente',0);
 /*!40000 ALTER TABLE `secret_group` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -203,4 +127,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-21  9:27:29
+
